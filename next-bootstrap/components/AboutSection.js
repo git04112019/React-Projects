ac7a80d18487ef0b19aa7f787f46321c88825e54
Hans <bmm79@nau.edu>
2019-05-28 13:16:58 -0700

    rework skill section with a chart component

--- next-bootstrap/components/AboutSection.js
+++ next-bootstrap/components/AboutSection.js
@@ -1,9 +1,52 @@
 import React from 'react';
-
-
+import { 
+  ResponsiveContainer, 
+  RadarChart, 
+  PolarGrid, 
+  PolarAngleAxis, 
+  PolarRadiusAxis, 
+  Radar, 
+  Legend 
+} 
+from 'recharts';
+const data = [
+  {
+    "subject": "Html",
+    "A": 100,
+    "fullMark": 100
+  },
+  {
+    "subject": "CSS",
+    "A": 98,
+    "fullMark": 100
+  },
+  {
+    "subject": "JS",
+    "A": 80,
+    "fullMark": 100
+  },
+  {
+    "subject": "PHP",
+    "A": 80,
+    "fullMark": 100
+  },
+  {
+    "subject": "Learning",
+    "A": 99,
+    "fullMark": 100
+  },
+  {
+    "subject": "DevOp",
+    "A": 65,
+    "fullMark": 100
+  }
+]
 
 const AboutSection = () => (
   <React.Fragment>   
+
+
+
       <section id="about" className="about-mf sect-pt4 route">
         <div className="container">
           <div className="row">
@@ -29,31 +72,15 @@ const AboutSection = () => (
                     </div>
                     <div className="skill-mf">
                       <p className="title-s">Skills</p>
-                      <span>HTML/CSS</span> <span className="pull-right">99%</span>
-                      <div className="progress">
-                        <div className="progress-bar" role="progressbar" style={{width: '99%'}} aria-valuenow={99} aria-valuemin={0} aria-valuemax={100} />
-                      </div>
-                      <span>JAVASCRIPT</span> <span className="pull-right">90%</span>
-                      <div className="progress">
-                        <div className="progress-bar" role="progressbar" style={{width: '90%'}} aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
-                      </div>
-                      <span>PHP</span> <span className="pull-right">50%</span>
-                      <div className="progress">
-                        <div className="progress-bar" role="progressbar" style={{width: '50%'}} aria-valuenow={50} aria-valuemin={0} aria-valuemax={100} />
-                      </div>
-                      <span>WordPress/Custom CMS</span> <span className="pull-right">90%</span>
-                      <div className="progress">
-                        <div className="progress-bar" role="progressbar" style={{width: '90%'}} aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
-                      </div>
-                      <span>DevOp/Cloud Ops</span> <span className="pull-right">90%</span>
-                      <div className="progress">
-                        <div className="progress-bar" role="progressbar" style={{width: '90%'}} aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
-                      </div>
-                      <span>Much more</span> <span className="pull-right">100%</span>
-                      <div className="progress">
-                        <div className="progress-bar" role="progressbar" style={{width: '100%'}} aria-valuenow={100} aria-valuemin={0} aria-valuemax={100} />
-                      </div>
-                      {/*  */}
+                      <ResponsiveContainer width={300} minWidth={200} height={300}>
+                          <RadarChart outerRadius={90} width={300} height={250} data={data}>
+                            <Radar name="Skills" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                            <PolarGrid />
+                            <PolarAngleAxis dataKey="subject" />
+                            <PolarRadiusAxis angle={30} domain={[0, 100]} />
+                            <Legend />
+                          </RadarChart>
+                      </ResponsiveContainer>
                     </div>
                   </div>
                   <div className="col-md-6">
@@ -73,7 +100,7 @@ const AboutSection = () => (
                       <p className="lead">
                         I try to live passionately which is way when I get things done and here are just a few examples.
                         <br />
-                        Coding - I've build multiple CLI tools for fun. Most of it pretty hacky but they've gotten the job done. Check out my github.
+                        Coding - I've built multiple CLI tools for fun. Most of it pretty hacky but they've gotten the job done. Check out my github.
                         <br />
                         Make/Build - I don't just code and game, I make stuff like metal smelters, jewerly for wife, simple circuit boards and other fun stuff.
                         <br />
@@ -90,6 +117,15 @@ const AboutSection = () => (
 
 
   <style jsx>{`
+
+    .fa{
+      font-size: 2em;
+      margin: auto 5px;
+    }
+    a.fa.fa-linkedin-square{
+      color: #0078ff;
+    }
+
     .about-mf .box-shadow-full {
       padding-top: 4rem;
       padding-bottom: 4rem;
